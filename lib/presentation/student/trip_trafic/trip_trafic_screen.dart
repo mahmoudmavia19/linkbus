c5d7931fbbb846e1f83802905ed81eb558e5495d
mahmoudmavia19 <hamdymhmoud31@gamil.com>
2024-02-08 15:25:31 +0200

    student UI

--- lib/presentation/student/trip_trafic/trip_trafic_screen.dart
+++ lib/presentation/student/trip_trafic/trip_trafic_screen.dart
@@ -16,12 +16,25 @@ class TripTraficScreen extends GetWidget<TripTraficController> {
             title: Text(AppStrings.remaining,style: TextStyle(
                 fontWeight: FontWeight.bold
             ),),
-            subtitle:Row(
+            subtitle:Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Icon(Icons.timer,color: theme.primaryColor,),
-                SizedBox(width: 10.0,),
-                Obx(()=> Expanded(child: Text(controller.timer.value))),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.timer,color: theme.primaryColor,),
+                    SizedBox(width: 10.0,),
+                    Obx(()=> Expanded(child: Text(controller.timer.value))),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.social_distance,color: theme.primaryColor,),
+                    SizedBox(width: 10.0,),
+                    Obx(()=> Expanded(child: Text('${controller.newDistance.value}m'))),
+                  ],
+                ),
               ],
             ),
             trailing: TextButton(onPressed: (){
@@ -31,19 +44,54 @@ class TripTraficScreen extends GetWidget<TripTraficController> {
           Expanded(
             child: Container(
                 height: 200,
-                child: GoogleMap(
-                  mapType: MapType.terrain,
-                  myLocationEnabled:true ,
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(
-                      controller.currentLocation_.value.latitude ?? startMapLocation.latitude,
-                      controller.currentLocation_.value.longitude ?? startMapLocation.longitude,
+                child: Obx(
+                      () => GoogleMap(
+                    mapType: MapType.terrain,
+                    myLocationEnabled: true,
+                    circles: {
+                      Circle(
+                        circleId: const CircleId("start"),
+                        center: LatLng(
+                          startMapLocation.latitude,
+                          startMapLocation.longitude,
+                        ),
+                        radius: 2,
+                      )
+                    },
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(startMapLocation.latitude, startMapLocation.longitude),
+                      zoom: 19,
                     ),
-                    zoom: 16,
+                    onMapCreated: (_controller) {
+                      controller.mapController = _controller;
+                    },
+                    polylines: {
+                      Polyline(
+                        polylineId: const PolylineId("route"),
+                        points: controller.polylineCoordinates,
+                        color: theme.primaryColor,
+                        width: 6,
+                      ),
+                    },
+                    onTap: (argument) {
+                      print(argument);
+                    },
+                    markers: {
+                      Marker(
+                        markerId: MarkerId('Start Location'),
+                        position: LatLng(controller.startLocation.value.latitude ?? startMapLocation.latitude,
+                            controller.startLocation.value.longitude ?? startMapLocation.longitude),
+                        infoWindow: InfoWindow(title: 'Start Location'),
+                      ),
+                      Marker(
+                        markerId: MarkerId('End Location'),
+                        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+                        position: LatLng(controller.endLocation.value.latitude ?? startMapLocation.latitude,
+                            controller.endLocation.value.longitude ?? startMapLocation.longitude),
+                        infoWindow: InfoWindow(title: 'End Location'),
+                      ),
+                    },
                   ),
-                  onMapCreated: (_controller) {
-                    controller.mapController = _controller ;                    // Do other initialization as needed
-                  },
                 ),
               )
           ),
