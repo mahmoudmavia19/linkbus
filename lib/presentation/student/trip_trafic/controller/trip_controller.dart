48a1b7cd48b6e1548a2cad24b66d999e1f65e565
mahmoudmavia19 <hamdymhmoud31@gamil.com>
2024-02-09 21:29:39 +0200

    student UI 2

--- lib/presentation/student/trip_trafic/controller/trip_controller.dart
+++ lib/presentation/student/trip_trafic/controller/trip_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:intl/intl.dart';
 import 'package:location/location.dart';
 import '../../../../core/app_export.dart';
 import '../../../../core/constants/constant.dart';
@@ -43,33 +42,7 @@ class TripTraficController extends GetxController {
      super.onInit();
   }
 
-/*
-  _timerTest(){
-    var startTrip = DateTime(0,0,0,0,0,20);
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      this.timer.value = DateFormat.ms().format(startTrip.subtract(Duration(seconds: timer.tick)));
-      print(timer.tick);
-      if( timer.tick == 3 ){
-        Get.snackbar('Bus Notification', 'Your bus has start trip',duration: Duration(seconds: 2),
-        onTap: (snack) {
-          Get.closeAllSnackbars();
-        },);
-      }else if (timer.tick == 10){
-        Get.snackbar('Bus Notification', 'Your bus has Very close ',duration: Duration(seconds: 2),
-          onTap: (snack) {
-            Get.closeAllSnackbars();
-          },);
-      }
-      else if (timer.tick == 20){
-        Get.snackbar('Bus Notification', 'Your bus has arrived',duration: Duration(seconds : 2),
-          onTap: (snack) {
-            Get.closeAllSnackbars();
-          },);
-        timer.cancel();
-      }
-    });
-  }
-*/
+
   void startUpdatingLocations() {
     startLocation.value = LocationData.fromMap({
       'latitude': startMapLocation.latitude,
